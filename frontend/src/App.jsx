f322ce0a080a604953ebef5759b24d2a420c0130
unknown <mahendraakhila619@gmail.com>
2025-04-08 12:23:52 +0530

    1223

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import Dashboard from "./pages/Dashboard";
+import Footer from "./components/Footer";
+import Vnrheader from "./components/Vnrheader";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Vnrheader />
+      <div className="flex-1 overflow-auto">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
